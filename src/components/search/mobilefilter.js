8fa533cab7e26927c5e315bc8d68050b493a3104
Aman Bhargava <amanbhargava2001@gmail.com>
2021-06-24 19:53:35 +0530

    Added Mobile Nav In/Out

--- src/components/search/mobilefilter.js
+++ src/components/search/mobilefilter.js
@@ -2,16 +2,24 @@ import DropdownRefinementList from "./dropdown"
 import "./dropdownstyles.css"
 import React from "react"
 function Mobilebar() {
+    let myID = document.getElementById("bar");
+    let height = document.documentElement.scrollHeight;
+    let myScrollFunc = function() {
+      let y = window.scrollY;
+      if (y >= 200 && y <= (height-574*2)) {
+        myID.className = "block"
+      } else {
+        myID.className = "hidden"
+      }
+    };
+    
+    window.addEventListener("scroll", myScrollFunc);
   return (
-    <div class="lg:hidden md:hidden block w-full">
+    <div class="lg:hidden md:hidden  block w-full">
       <section
-        id="bottom-navigation"
-        class="md:hidden block w-full fixed inset-x-0 bottom-0 z-10 bg-accent-gray shadow"
-      >
-        <section
-          id="bottom-navigation"
-          class="block fixed w-full inset-x-0 bottom-0 z-10 bg-accent-gray shadow"
-        >
+        id="bar"
+        class="md:hidden block w-full fixed inset-x-0 bottom-0 z-10 bg-accent-gray shadow">
+        <section class="block fixed w-full inset-x-0 bottom-0 z-10 bg-accent-gray shadow">
           <div id="tabs" class="flex justify-between">
             <span class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
               <span class="tab tab-kategori block text-xs text-light-gray">
