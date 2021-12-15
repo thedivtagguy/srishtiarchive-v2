0e5186b97811fde3f1a57a870afe102c4872b154
Aman Bhargava <amanbhargava2001@gmail.com>
2021-12-15 21:16:19 +0530

    Add Mobile Search Menu

--- src/components/search/SearchBox.jsx
+++ src/components/search/SearchBox.jsx
@@ -3,17 +3,23 @@ import SearchGrid2 from "./searchV2"
 import MobileSearch from "./searchV2Mobile"
 
 const Search = () => {
-
-    // If the user is on a mobile device, display MobileSearch
-    // Otherwise, display SearchGrid2
+    // Check if the user is on a mobile device
+    
+    let isMobile = false;
     if (typeof window !== "undefined") {
-        if (window.innerWidth <= 768) {
-            return(
-                 <MobileSearch />)
-        } else {
-            return <SearchGrid2 />
-        }
+         isMobile = window.innerWidth < 768
     }
+    
+    return (
+        <div>
+            {isMobile ? (
+                <MobileSearch />
+            ) : (
+                <SearchGrid2 />
+            )}
+        </div>
+    )
 }
+
 
 export default Search
